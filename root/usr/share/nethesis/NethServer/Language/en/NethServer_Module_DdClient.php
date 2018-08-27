b21bfa355213baefaa954406cc895c81b4956cc4
Stephane de Labrusse <stephdl@de-labrusse.fr>
2018-08-27 22:36:58 +0200

    DYNU added in translation

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_DdClient.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_DdClient.php
@@ -38,3 +38,4 @@ $L['DynDns_Provider_Title'] = 'Dynamic DNS Providers';
 $L['DynDns_SELFHOST_label'] = 'Selfhost (selfhost.eu) dynamic adresses';
 $L['DynDns_CLOUDFLARE_label'] = 'Cloudflare (cloudflare.com) dynamic adresses';
 $L['DynDns_STRATO_label'] = 'Strato (strato.com) dynamic adresses';
+$L['DynDns_DYNU_label'] = 'Dynu (dynu.com) dynamic adresses';
