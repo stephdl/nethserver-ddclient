c1f1d0b7eb8619ac9e8f69bd0662a9c914a7b830
Stephane de Labrusse <stephdl@de-labrusse.fr>
2019-11-23 10:56:52 +0100

    About url point to stephdl github

--- ui/src/views/About.vue
+++ ui/src/views/About.vue
@@ -16,11 +16,11 @@
 
       <div class="list-group-item info-item">
         <span class="fa fa-globe m-right-sm"></span>
-        <a target="_blank" href="https://github.com/NethServer/nethserver-ddclient">{{$t('about.website')}}</a>
+        <a target="_blank" href="https://github.com/stephdl/nethserver-ddclient">{{$t('about.website')}}</a>
       </div>
       <div class="list-group-item info-item">
         <span class="fa fa-bug m-right-sm"></span>
-        <a target="_blank" href="https://github.com/NethServer/dev/issues">{{$t('about.bug_tracker')}}</a>
+        <a target="_blank" href="https://github.com/stephdl/dev/issues">{{$t('about.bug_tracker')}}</a>
       </div>
 
       <div class="list-group-item info-item">
